0fbf10042f2244f47e7cb5b8287074389d5ab51f
Conall O Maolain <conallomaolain1@gmail.com>
2024-05-14 16:21:25 +0200

    add form content

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,3 +1,14 @@
-export function Form() {
-  return <h1>Form</h1>;
+export function Form(onAddActivity) {
+  return (
+    <>
+      <h1>Add new Activity</h1>
+      <form>
+        <label htmlFor="text">Name: </label>
+        <input type="text" /> <br />
+        <label htmlFor="checkbox">Good-weather activity</label>
+        <input type="checkbox" /> <br />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
 }
