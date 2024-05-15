aba5c13d008b3fd6d8c0ab6f54701fb9ab4f40c4
Conall O Maolain <conallomaolain1@gmail.com>
2024-05-15 11:49:31 +0200

    fix activities bug and add local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./App.css";
 import { Form } from "./components/Form.jsx";
 import { List } from "./components/List.jsx";
+import useLocalStorageState from "use-local-storage-state";
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useLocalStorageState("activities", {
+    defaultValue: [],
+  });
 
   function handleAddActivity(newActivity) {
     setActivities([...activities, newActivity]);
   }
 
-  console.log(activities);
+  console.log("activities", activities);
   return (
     <>
       <Form onAddActivity={handleAddActivity} />
-      <List />
+      <List activities={activities} />
     </>
   );
 }
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,7 +1,14 @@
-export function List({ onAddActivityToList }) {
-  <>
-    <ul></ul>
-  </>;
+import useLocalStorageState from "use-local-storage-state";
+
+export function List({ activities }) {
+  console.log(activities);
+  return (
+    <ul>
+      {activities.map((activity) => (
+        <li key={activity.id}>{activity.name}</li>
+      ))}
+    </ul>
+  );
 }
 
 //
